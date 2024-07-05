f2d234ae0f4a8e28a32ce9058cd4da3b8d471041
umair khan <explain816@gmail.com>
2024-07-05 20:39:07 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import user from './images/user.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={user} className='img-one'></img>
+      <img src={user} className="img-two"></img>
+    </div>
+  );
+}
+
+export default App;
